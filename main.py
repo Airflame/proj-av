ec5392eb82d965cd167c53c7b28aa2e3fe886122
airflame <airflame@wp.pl>
2022-10-11 22:33:29 +0200

    Added music playing

--- main.py
+++ main.py
@@ -18,6 +18,35 @@ def _get_intent() -> discord.Intents:
     return intents
 
 
+NOTES = {"c1": 130.81,
+         "c#1": 138.59,
+         "d1": 146.83,
+         "d#1": 155.56,
+         "e1": 164.81,
+         "f1": 174.61,
+         "f#1": 185.00,
+         "g1": 196.00,
+         "g#1": 207.65,
+         "a1": 220.00,
+         "a#1": 233.08,
+         "b1": 246.94,
+         "c2": 261.63,
+         "c#2": 277.18,
+         "d2": 293.66,
+         "d#2": 311.13,
+         "e2": 329.63,
+         "f2": 349.23,
+         "f#2": 369.99,
+         "g2": 392.00,
+         "g#2": 415.30,
+         "a2": 440.00,
+         "a#2": 466.16,
+         "b2": 493.88,
+         "c3": 523.25}
+
+FULL_NOTE_LEN = 600
+
+
 class Client(discord.Client):
     def __init__(self, token: str, guild_name: str) -> None:
         super().__init__(intents=_get_intent())
@@ -38,6 +67,8 @@ class Client(discord.Client):
         elif message.content.startswith("!beep"):
             freq = int(message.content.split(" ")[-1])
             await self.sound(freq)
+        elif message.content.startswith("!music"):
+            await self.music(message.content.split(" ")[-1])
 
     async def info(self) -> None:
         await self._guild.text_channels[1].send("ProjAvTest")
@@ -48,6 +79,17 @@ class Client(discord.Client):
         print("Beeped {} Hz".format(freq))
         os.system("env -i beep -f {} -l 500".format(freq))
         await self._guild.text_channels[1].send("Beeped")
+
+    async def music(self, notes_str: str) -> None:
+        notes = notes_str.split("-")
+        for note in notes:
+            note_pitch, note_len = note.split(";")
+            pitch = NOTES.get(note_pitch)
+            freq = min(FULL_NOTE_LEN/float(eval(note_len)), 1000)
+            freq = max(freq, 100)
+            print("Played note {}".format(note_pitch))
+            os.system("env -i beep -f {} -l {}".format(pitch, freq))
+        await self._guild.text_channels[1].send("Played music")
 
 
 client = Client(DISCORD_TOKEN, DISCORD_GUILD)
